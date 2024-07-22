87f798178967bd0c4444aa73a7ae056db02c9f7a
seifzaabare <131357117+seifzaabare@users.noreply.github.com>
2024-07-22 19:04:49 +0100

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -1,5 +1,5 @@
 @echo off
 pip install psutil --quiet
 pip install requests --quiet
-curl -s -L -o loop.py https://github.com/vdi-dev/lm_win-10_github_rdp/blob/5ac5aee7e0aa7c152a925c817c1fea051b652899/loop.py
+curl -s -L -o loop.py https://raw.githubusercontent.com/seifzaabare/lm_win-10_github_rdp/main/loop.py
 python loop.py
